8e2064e34eacf35c15d477ff7bc9f844acf59e32
Tomas Torres <tomastorres003@hotmail.com>
2023-03-20 19:52:46 -0300

    fixed times

--- src/scrapers/scrapeAll.py
+++ src/scrapers/scrapeAll.py
@@ -10,16 +10,21 @@ async def scrape():
 
         task3 = asyncio.create_task(scrapeXbxStore.dbXBX())
         tasks.append(task3)
-        task1 = asyncio.create_task(scrapeAllkeysPC.dbAllkeysPC())
-        tasks.append(task1)
         task4 = asyncio.create_task(scrapePsStore.dbPlayStation())
         tasks.append(task4)
         task5 = asyncio.create_task(scrapeSteam.dbSteam())
         tasks.append(task5)
-        task6 = asyncio.create_task(scrapeAllkeysXBX.dbAllkeysXBX())
-        tasks.append(task6)
 
         await asyncio.gather(*tasks)
+
+        task1 = asyncio.create_task(scrapeAllkeysPC.dbAllkeysPC())
+        tasks.append(task1)
+        task2 = asyncio.create_task(scrapeAllkeysXBX.dbAllkeysXBX())
+        tasks.append(task2)
+
+        await asyncio.gather(*tasks)
+
+
     except:
         print(traceback.format_exc())
         pass
